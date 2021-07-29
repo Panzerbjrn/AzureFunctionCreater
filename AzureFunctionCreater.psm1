f1c80b3ca5f83441947971ebd200dc4de7f540f0
Lars Panzerbjørn <lars@panzerbjrn.eu>
2021-07-29 21:27:21 +0200

    Updating

---
+++ AzureFunctionCreater.psm1
@@ -0,0 +1,44 @@
+﻿#region Script Header
+#	Thought for the day:
+#	NAME: $($ModuleName).psm1
+#	AUTHOR: $($Author)
+#	CONTACT: GitHub: Panzerbjrn / Twitter: Panzerbjrn
+#	DATE: $($Date)
+#	VERSION: 0.1 - $($Date) - Module Created with Create-NewModuleStructure by Lars Panzerbjørn
+#
+#	SYNOPSIS:
+#
+#
+#	DESCRIPTION:
+#	$($Description)
+#
+#	REQUIREMENTS:
+#
+#endregion Script Header
+
+#Requires -Version 4.0
+
+[cmdletbinding()]
+param()
+
+Write-Verbose $PSScriptRoot
+
+#Get public and private function definition files.
+$Functions  = @( Get-ChildItem -Path $PSScriptRoot\Functions\*.ps1 -ErrorAction SilentlyContinue )
+$Helpers = @( Get-ChildItem -Path $PSScriptRoot\Helpers\*.ps1 -ErrorAction SilentlyContinue )
+
+#Dot source the files
+Foreach ($Import in @($Functions + $Helpers))
+{
+	Try
+	{
+		Write-Verbose "Processing $($Import.Fullname)"
+		. $Import.Fullname
+	}
+	Catch
+	{
+		Write-Error -Message "Failed to Import function $($Import.Fullname): $_"
+	}
+}
+
+Export-ModuleMember -Function $Functions.Basename
---
+++ Functions/Create-AzureFunction.ps1
@@ -0,0 +1,96 @@
+﻿Function Create-AzureFunction
+{
+<#
+	.SYNOPSIS
+		This will create an Azure Function.
+
+	.DESCRIPTION
+		This will create an Azure Function and all attendant files\services.
+
+	.EXAMPLE
+		Give an example of how to use it
+
+	.EXAMPLE
+		Give another example of how to use it
+
+	.PARAMETER BaseName
+		This will be the base for Resource Groups, Storage Accounts, etc.
+		This must be between 3 and 20 characters due to Azure limitations.
+
+	.PARAMETER Location
+		This will be the Azure regional location
+
+	.PARAMETER StorageSKU
+		This will be the type or Storage required
+
+	.PARAMETER CodePath
+		This will be the location of the code.
+
+	.PARAMETER Logfile
+		This will be the location of the logfile.
+
+	.INPUTS
+		Input is from command line or called from a script.
+
+	.OUTPUTS
+		This will output a logfile and a Function App.
+
+	.NOTES
+		Version:			0.1
+		Author:				Lars Panzerbjørn
+		Creation Date:		2019.10.24
+		Purpose/Change: Initial script development
+#>
+	[CmdletBinding(SupportsShouldProcess=$True,ConfirmImpact='Low')]
+	param
+	(
+		[Parameter(Mandatory,
+			ValueFromPipeline=$True,
+			ValueFromPipelineByPropertyName=$True,
+			HelpMessage='What base name would you like to use?')]
+		[ValidateLength(3,20)]
+		[string[]]$BaseName,
+
+		[Parameter(
+			ValueFromPipeline=$True,
+			ValueFromPipelineByPropertyName=$True,
+			HelpMessage='What location would you like to use?')]
+		[string[]]$Location = "northeurope",
+
+		[Parameter(
+			ValueFromPipeline=$True,
+			ValueFromPipelineByPropertyName=$True,
+			HelpMessage='What location would you like to use?')]
+		[string[]]$StorageSku = 'Standard_LRS',
+
+		[Parameter()]
+		[string]$Logfile = 'errors.txt'
+	)
+
+	BEGIN
+	{
+		Write-Verbose "Creating log $LognPath"
+		Write-LogFile -LogFilePath $Logfile -Message "Processing Begins"
+		$RGName = $BaseName + "RG"
+		Write-Verbose "Resource Group name is $RGName"
+		$StorageAccountName = $BaseName + "SA"
+		
+	}
+
+	PROCESS
+	{
+		Write-Verbose "Beginning process loop"
+		
+		Write-Verbose "Registering Providers"
+		@('Microsoft.Web', 'Microsoft.Storage') | ForEach-Object {Register-AzResourceProvider -ProviderNamespace $_}
+
+		Write-Verbose "Attempting to create Azure Resource Group $$RGName"
+		Write-LogFile -LogFilePath $Logfile -Message "Attempting to create Azure Resource Group"
+		$ResourceGroup = Get-AzResourceGroup -Name $RGName -ErrorAction SilentlyContinue
+
+	}
+	END
+	{
+		Write-Verbose "Ending $($MyInvocation.Mycommand)"
+	}
+}
